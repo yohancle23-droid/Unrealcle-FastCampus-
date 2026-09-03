eb83bf307e102b0ca1c19be04ae3d161de027dab
SONGJOHAAN_\yohan <yohancle23@gmail.com>
2026-09-03 23:38:40 +0900

    프로젝트 셋팅 [Part1 CH  03-1 부터]

---
+++ AnimProject/Source/AnimProject/AnimProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnimProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMPROJECT_API AAnimProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
